9b9e820315b720e83e23a4ee398f4e6c3beab071
Md Irfan <iamhero337@gmail.com>
2025-01-02 11:38:28 +0530

    Added url path and in the store and the main projects url.py files and also made view for category and product.

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Category, Product
 
-
 def category_list(request):
     categories = Category.objects.all()
-    return render(request, 'store/category_list.html')
+    return render(request, 'store/category_list.html', {'categories': categories})
 
+def product_list(request, category_id=None):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
